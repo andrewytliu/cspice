a7620e81f352636e81bf2c3cb6ea932cf36a6aeb
Andrew Liu <andrewliu33@gmail.com>
2010-12-23 15:43:32 +0800

    Moving header file

---
+++ src/lib/element.h
@@ -0,0 +1,79 @@
+#ifndef __ELEMENT_H__
+#define __ELEMENT_H__
+
+#include <string>
+using namespace std;
+/*
+ * This is the base type of Resistor, Capacitor, Inductor
+ */
+class Element {
+public:
+   virtual string type() const      = 0 ; // nothing special, might be used when debugging
+   virtual string formula() const   = 0 ; // the formula of Z
+   virtual int order() const        = 0 ; // R: 0, L: -1, C: 1
+   virtual double value() const     = 0 ; // the admittance = s^(order()) * value()
+   string name() const { return this->_name; }
+protected:
+   double _value ;
+   string _name ;
+};
+
+class Capacitor : public Element {
+public:
+   virtual string type() const {
+      return "Capacitor" ;
+   }
+
+   virtual string formula() const {
+      return "s * " + this->_name ;
+   }
+
+   virtual int order() const {
+      return 1;
+   }
+
+   virtual double value() const {
+      return this->_value ;
+   }
+};
+
+class Inductor : public Element {
+public:
+   virtual string type() const {
+      return "Inductor" ;
+   }
+
+   virtual string formula() const {
+      return "1 / (s * " + this->_name + ")" ;
+   }
+
+   virtual int order() const {
+      return 0;
+   }
+
+   virtual double value() const {
+      return 1.0 / this->_value ;
+   }
+};
+
+class Resistor : public Element {
+public:
+   virtual string type() const {
+      return "Resistor" ;
+   }
+
+   virtual string formula() const {
+      return "1 / " + this->_name ;
+   }
+
+   virtual int order() const {
+      return 0;
+   }
+
+   virtual double value() const {
+      return 1.0 / this->_value ;
+   }
+};
+
+#endif /* __ELEMENT_H__ */
+
---
+++ src/lib/tree.h
@@ -0,0 +1,31 @@
+#ifndef __TREE_H__
+#define __TREE_H__
+
+#include "element.h"
+#include <vector>
+using namespace std;
+
+class Tree ;
+class Node ;
+class Connection ;
+
+/**  Class definitions **/
+
+class Tree {
+   vector<Node> nodes ;
+};
+
+class Node {
+public:
+   unsigned nodeId ; // used to identify
+   vector<Connection> connections ;
+};
+
+class Connection {
+public:
+   Node * destination ;
+   Element * element ;
+};
+
+#endif /* __TREE_H__ */
+
